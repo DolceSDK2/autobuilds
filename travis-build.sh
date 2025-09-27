39f774767bbcb632b807e37ebef74cbafb4c45b0
SSMG4 <matthew.gallon0912@gmail.com>
2025-09-27 16:04:32 +0200

    Update Repos

--- travis-build.sh
+++ travis-build.sh
@@ -2,4 +2,4 @@
 
 TAG="build-$(date -u '+%Y-%m-%d-%H%M%S')"
 git tag -s -m "${TAG}" "${TAG}"
-git push "git@github.com:DolceSDK/autobuilds.git" "${TAG}"
+git push "git@github.com:DolceSDK2/autobuilds.git" "${TAG}"
